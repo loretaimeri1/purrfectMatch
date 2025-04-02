a345b25f88e5b593cf0c541db6df63399d59bf30
Loreta Imeri <loreta.imeri@student.uni-pr.edu>
2025-04-02 18:09:30 +0200

    pets

--- class/Pet.php
+++ class/Pet.php
@@ -141,5 +141,102 @@ class Pet extends Database{
     {
         $this->photoImage = $photoImage;
     }
+
+    public function find_specie($specieid){
+        $sql="SELECT * FROM ". static::$db_table ." WHERE specieid=:specieid";
+        $stmt=$this->prepare($sql);
+        $stmt->bindParam(':specieid',$specieid);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS, __NAMESPACE__ . "\\Pet");
+        return $stmt->fetchAll();
+    }
+
+    public function find_gender($gender){
+        $sql="SELECT * FROM ". static::$db_table ." WHERE gender=:gender";
+        $stmt=$this->prepare($sql);
+        $stmt->bindParam(':gender',$gender);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS, __NAMESPACE__ . "\\Pet");
+        return $stmt->fetchAll();
+    }
+
+    public function find_gender_species($gender,$specieid){
+        $sql="SELECT * FROM ". static::$db_table ." WHERE gender=:gender AND specieid=:specieid";
+        $stmt=$this->prepare($sql);
+        $stmt->bindParam(':gender',$gender);
+        $stmt->bindParam(':specieid',$specieid);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS, __NAMESPACE__ . "\\Pet");
+        return $stmt->fetchAll();
+    }
+
+    public function create(){
+        try{
+            $this->startupLoad($this->photoImage);
+            $this->image = $this->filename;
+            $isFileUploaded = $this->uploadFile(); 
+            if($isFileUploaded){
+                parent::create();
+            }
+            else{
+                foreach($this->errors as $error){
+                    echo $error . "<br>";
+                }
+            }
+        }
+        catch(Exception $e){
+            echo "Pet could not be added" . $e->getMessage();
+        }
+    }
+
+    public function update(){
+        try {
+           if(isset($this->photoImage)){
+            $this->uploadfile = $this->src . $this->image;
+            if(!!$this->uploadfile){
+                unlink($this->uploadfile);
+            }
+            $this->startupLoad($this->photoImage);
+            $this->image = $this->filename;
+            $isFileUploaded = $this->uploadFile();
+            if($isFileUploaded){
+                if(parent::update()){
+                    return true;
+                }
+            }
+            else{
+                foreach($this->errors as $error){
+                    echo $error . "<br>";
+                }
+            }
+           }
+           else{
+                if(parent::update()){
+                    return true;
+                }
+            }
+        }
+        catch(Exception $e){
+            echo "Pet could not be modified" . $e->getMessage();
+        } 
+    }
+
+    public function delete(){
+        try{
+            if(parent::delete()){
+                $this->uploadfile = $this->src . $this->image;
+                if(file_exists($this->uploadfile)){
+                    unlink($this->uploadfile); 
+                }
+                return true;
+            }
+            else{
+                return false; 
+            }
+        }
+        catch(Exception $e){
+            echo "Pet could not be deleted" . $e->getMessage();
+        }
+    }
 }
 ?>
